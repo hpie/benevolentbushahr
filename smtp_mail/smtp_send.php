2211611e072a2fd8c59ca532884f3057cf6f2fcc
HPIE <sunil@hpie.in>
2022-03-11 22:30:43 +0530

    corrected spelling mistake

--- smtp_mail/smtp_send.php
+++ smtp_mail/smtp_send.php
@@ -76,12 +76,13 @@ class SMTP_mail {
 
         $this->mail->setFrom($this->sender_email,"Benevolent Bushahr");
 
-        $this->mail->addAddress("info@benevolentbushahr.in");
+        $this->mail->addAddress("benevolentbushahr@gmail.com");
+		$this->mail->addBCC("info@benevolentbushahr.in");
 
         $this->mail->Subject = $this->subject;
 
         $this->mail->isHTML(true);                                  // Set email format to HTML                                   
-        $this->mail->Body = 'Name: '.$data['first-name'].' '.$data['last-name']. '<br> Mobile No: ' .$data['mobile-no']. '<br> Email: '.$data['email-id']. '<br> Current profession: '.$data['current-profession']. '<br> Volunteering Interest Shown: '. $data['select-sort'].' <br> &nbsp; &nbsp; Profession : '.$data['select-profession'].' <br>  &nbsp; &nbsp; &nbsp; &nbsp; Sub-Profession : '.$data['select-subprofession']. '<br> Please contact the volunteer ';
+        $this->mail->Body = 'Name: '.$data['firstname'].' '.$data['lastname']. '<br> Mobile No: ' .$data['mobileno']. '<br> Email: '.$data['emailid']. '<br> Current profession: '.$data['currentprofession']. '<br> Volunteering Interest Shown: '. $data['sort'].' <br> &nbsp; &nbsp; Profession : '.$data['profession'].' <br>  &nbsp; &nbsp; Sub-Profession : '.$data['subprofession']. '<br> Please contact the volunteer ';
 
         $this->mail->AltBody = "";
 
@@ -129,11 +130,12 @@ class SMTP_mail {
         $this->mail->setFrom($this->sender_email,"Benevolent Bushahr");
 
         $this->mail->addAddress($data['email-id']);
+		$this->mail->addBCC("info@benevolentbushahr.in");
 
         $this->mail->Subject = $this->subject;
 
         $this->mail->isHTML(true);                                  // Set email format to HTML                                   
-        $this->mail->Body = 'Thank you '. $data['first-name'].' '.$data['last-name']. '<br> We have received your intereset for volunteering and will contact back soon <br> Interest showen in : '. $data['select-sort'].' <br> &nbsp; &nbsp; Profession : '.$data['select-profession'].' <br>  &nbsp; &nbsp; &nbsp; &nbsp; Sub-Profession : '.$data['select-subprofession'];
+        $this->mail->Body = 'Thank you '. $data['firstname'].' '.$data['lastname']. '<br> We have received your intereset for volunteering and will contact back soon <br> Interest showen in : '. $data['sort'].' <br> &nbsp; &nbsp; Profession : '.$data['profession'].' <br>  &nbsp; &nbsp; &nbsp; &nbsp; Sub-Profession : '.$data['subprofession'];
 
         $this->mail->AltBody = "";
 
@@ -150,6 +152,113 @@ class SMTP_mail {
 //            print_r($resultMail);die;
             return $resultMail;
         }
-    } 
+    }
+
+
+public function sendDonationMail($data) {       
+        
+        $this->sender_email ='noreply@benevolentbushahr.in';
+
+        $this->sender_name ='Benevolent Bushahr';
+
+        $this->subject ='New Donation Interest Received';
+
+        $this->mail->isSMTP();
+
+        $this->mail->SMTPDebug = 0;
+
+        $this->mail->Debugoutput = 'html';
+
+        $this->mail->Host = $this->host;
+
+        $this->mail->Port = $this->port;
+
+        $this->mail->SMTPAuth = true;
+
+        $this->mail->SMTPSecure = true;
+
+        $this->mail->Username = $this->username;
+
+        $this->mail->Password = $this->password;
+
+        $this->mail->setFrom($this->sender_email,"Benevolent Bushahr");
+
+        $this->mail->addAddress("benevolentbushahr@gmail.com");
+		$this->mail->addBCC("info@benevolentbushahr.in");
+
+        $this->mail->Subject = $this->subject;
+
+        $this->mail->isHTML(true);                                  // Set email format to HTML                                   
+        $this->mail->Body = 'Name: '.$data['firstname'].' '.$data['lastname']. '<br> Mobile No: ' .$data['mobileno']. '<br> Email: '.$data['emailid']. '<br> Donation Interest Shown: '. $data['sort'].' <br> &nbsp; &nbsp; Sub Sort : '.$data['subsort'].' <br>  &nbsp; &nbsp; Amount : '.$data['budget']. '<br> Please contact the Donor ';
+
+        $this->mail->AltBody = "";
+
+        $resultMail=array();
+        $resultMail['success']=0;
+        $res=$this->mail->send();        
+        if($res==1){
+//            print_r($res);die;
+            $resultMail['success']=1;
+            return $resultMail;
+        }else {
+//            print_r($res);die;
+            $resultMail['Error']="Mailer Error: " . $this->mail->ErrorInfo;
+//            print_r($resultMail);die;
+            return $resultMail;
+        }
+    }	
+	
+	public function sendDonerThankyouMail($data) {       
+        
+        $this->sender_email ='noreply@benevolentbushahr.in';
+
+        $this->sender_name ='Benevolent Bushahr';
+
+        $this->subject ='Thank you for Contacting Benevolent Bushahr';
+
+        $this->mail->isSMTP();
+
+        $this->mail->SMTPDebug = 0;
+
+        $this->mail->Debugoutput = 'html';
+
+        $this->mail->Host = $this->host;
+
+        $this->mail->Port = $this->port;
+
+        $this->mail->SMTPAuth = true;
+
+        $this->mail->SMTPSecure = true;
+
+        $this->mail->Username = $this->username;
+
+        $this->mail->Password = $this->password;
+
+        $this->mail->setFrom($this->sender_email,"Benevolent Bushahr");
+
+        $this->mail->addAddress($data['email-id']);
+		$this->mail->addBCC("info@benevolentbushahr.in");
+
+        $this->mail->Subject = $this->subject;
+
+        $this->mail->isHTML(true);                                  // Set email format to HTML                                   
+        $this->mail->Body = 'Thank you '. $data['name'].' '.$data['name']. '<br> We have received your intereset for donation and will contact back soon <br> Interest shown in : '. $data['sort'].' <br> &nbsp; &nbsp; Type : '.$data['subsort'].' <br>  &nbsp; &nbsp; Amount : '.$data['budget'];
+
+        $this->mail->AltBody = "";
+
+        $resultMail=array();
+        $resultMail['success']=0;
+        $res=$this->mail->send();        
+        if($res==1){
+//            print_r($res);die;
+            $resultMail['success']=1;
+            return $resultMail;
+        }else {
+//            print_r($res);die;
+            $resultMail['Error']="Mailer Error: " . $this->mail->ErrorInfo;
+//            print_r($resultMail);die;
+            return $resultMail;
+        }
+    }
 }
 ?>
